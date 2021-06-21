24bc33dd54a73ba7b7508a29eee618a5848e86d3
Philip Herron <philip.herron@embecosm.com>
2021-06-21 16:09:11 +0100

    Various fixes to finish off module support.
    When we are able to fully resolve a name in the AST without type resolution
    we must give an apropriate definition lookup. These definition structures
    are used as part of let pattern bindings so that the identifier patterns
    lookup back to the let stmt itself. This ensures items act the same as
    other names in the compiler.

--- gcc/rust/typecheck/rust-hir-type-check-toplevel.h
+++ gcc/rust/typecheck/rust-hir-type-check-toplevel.h
@@ -105,7 +105,7 @@ public:
   void visit (HIR::ModuleBodied &module) override
   {
     for (auto &item : module.get_items ())
-      TypeCheckItem::Resolve (item.get ());
+      TypeCheckTopLevel::Resolve (item.get ());
   }
 
   void visit (HIR::StructStruct &struct_decl) override
