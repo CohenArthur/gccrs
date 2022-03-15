193e8e35d2233b46c17957569740f32653ff5ffe
Arthur Cohen <arthur.cohen@embecosm.com>
2022-03-15 18:19:31 +0100

    borrow-checker: Add more visitors

--- gcc/rust/typecheck/rust-hir-borrow-checker.cc
+++ gcc/rust/typecheck/rust-hir-borrow-checker.cc
@@ -17,6 +17,7 @@
 // <http://www.gnu.org/licenses/>.
 
 #include "rust-hir-borrow-checker.h"
+#include "rust-hir-stmt.h"
 
 namespace Rust {
 namespace Resolver {
@@ -24,13 +25,55 @@ void
 BorrowChecker::resolve (HIR::Crate &crate)
 {
   for (auto &item : crate.items)
-    item->accept_vis (*this);
+    {
+      rust_debug ("[ARTHUR] %s", item->as_string ().c_str ());
+      item->accept_vis (*this);
+    }
+
+  polonius.compute ();
 }
 
 void
 BorrowChecker::visit (HIR::AssignmentExpr &assignment)
+{}
+
+void
+BorrowChecker::visit (HIR::LetStmt &let)
 {
-  polonius.define_var (assignment, assignment.get_expr ());
+  polonius.define_var (let, let.get_init_expr ());
+}
+
+void
+BorrowChecker::visit (HIR::IdentifierExpr &expr)
+{
+  polonius.var_used_at (expr);
+}
+
+void
+BorrowChecker::visit (HIR::BlockExpr &block)
+{
+  for (auto &stmt : block.get_statements ())
+    stmt->accept_vis (*this);
+
+  block.get_final_expr ()->accept_vis (*this);
+}
+
+void
+BorrowChecker::visit (HIR::Function &function)
+{
+  function.get_definition ()->accept_vis (*this);
+}
+
+void
+BorrowChecker::visit (HIR::ExprStmtWithoutBlock &expr)
+{
+  expr.get_expr ()->accept_vis (*this);
+}
+
+void
+BorrowChecker::visit (HIR::ExprStmtWithBlock &expr)
+{
+  expr.get_expr ()->accept_vis (*this);
 }
 } // namespace Resolver
 } // namespace Rust
--- gcc/rust/typecheck/rust-hir-borrow-checker.h
+++ gcc/rust/typecheck/rust-hir-borrow-checker.h
@@ -35,7 +35,138 @@ public:
    */
   void resolve (HIR::Crate &crate);
 
-  void visit (HIR::AssignmentExpr &assignment) override;
+  void visit (HIR::Lifetime &) override {}
+  void visit (HIR::LifetimeParam &) override {}
+  void visit (HIR::PathInExpression &) override {}
+  void visit (HIR::TypePathSegment &) override {}
+  void visit (HIR::TypePathSegmentGeneric &) override {}
+  void visit (HIR::TypePathSegmentFunction &) override {}
+  void visit (HIR::TypePath &) override {}
+  void visit (HIR::QualifiedPathInExpression &) override {}
+  void visit (HIR::QualifiedPathInType &) override {}
+  void visit (HIR::LiteralExpr &) override {}
+  void visit (HIR::BorrowExpr &) override {}
+  void visit (HIR::DereferenceExpr &) override {}
+  void visit (HIR::ErrorPropagationExpr &) override {}
+  void visit (HIR::NegationExpr &) override {}
+  void visit (HIR::ArithmeticOrLogicalExpr &) override {}
+  void visit (HIR::ComparisonExpr &) override {}
+  void visit (HIR::LazyBooleanExpr &) override {}
+  void visit (HIR::TypeCastExpr &) override {}
+  void visit (HIR::CompoundAssignmentExpr &) override {}
+  void visit (HIR::GroupedExpr &) override {}
+  void visit (HIR::ArrayElemsValues &) override {}
+  void visit (HIR::ArrayElemsCopied &) override {}
+  void visit (HIR::ArrayExpr &) override {}
+  void visit (HIR::ArrayIndexExpr &) override {}
+  void visit (HIR::TupleExpr &) override {}
+  void visit (HIR::TupleIndexExpr &) override {}
+  void visit (HIR::StructExprStruct &) override {}
+  void visit (HIR::StructExprFieldIdentifier &) override {}
+  void visit (HIR::StructExprFieldIdentifierValue &) override {}
+  void visit (HIR::StructExprFieldIndexValue &) override {}
+  void visit (HIR::StructExprStructFields &) override {}
+  void visit (HIR::StructExprStructBase &) override {}
+  void visit (HIR::CallExpr &) override {}
+  void visit (HIR::MethodCallExpr &) override {}
+  void visit (HIR::FieldAccessExpr &) override {}
+  void visit (HIR::ClosureExprInner &) override {}
+  void visit (HIR::ClosureExprInnerTyped &) override {}
+  void visit (HIR::ContinueExpr &) override {}
+  void visit (HIR::BreakExpr &) override {}
+  void visit (HIR::RangeFromToExpr &) override {}
+  void visit (HIR::RangeFromExpr &) override {}
+  void visit (HIR::RangeToExpr &) override {}
+  void visit (HIR::RangeFullExpr &) override {}
+  void visit (HIR::RangeFromToInclExpr &) override {}
+  void visit (HIR::RangeToInclExpr &) override {}
+  void visit (HIR::ReturnExpr &) override {}
+  void visit (HIR::UnsafeBlockExpr &) override {}
+  void visit (HIR::LoopExpr &) override {}
+  void visit (HIR::WhileLoopExpr &) override {}
+  void visit (HIR::WhileLetLoopExpr &) override {}
+  void visit (HIR::ForLoopExpr &) override {}
+  void visit (HIR::IfExpr &) override {}
+  void visit (HIR::IfExprConseqElse &) override {}
+  void visit (HIR::IfExprConseqIf &) override {}
+  void visit (HIR::IfExprConseqIfLet &) override {}
+  void visit (HIR::IfLetExpr &) override {}
+  void visit (HIR::IfLetExprConseqElse &) override {}
+  void visit (HIR::IfLetExprConseqIf &) override {}
+  void visit (HIR::IfLetExprConseqIfLet &) override {}
+  void visit (HIR::MatchExpr &) override {}
+  void visit (HIR::AwaitExpr &) override {}
+  void visit (HIR::AsyncBlockExpr &) override {}
+  void visit (HIR::TypeParam &) override {}
+  void visit (HIR::LifetimeWhereClauseItem &) override {}
+  void visit (HIR::TypeBoundWhereClauseItem &) override {}
+  void visit (HIR::Module &) override {}
+  void visit (HIR::ExternCrate &) override {}
+  void visit (HIR::UseTreeGlob &) override {}
+  void visit (HIR::UseTreeList &) override {}
+  void visit (HIR::UseTreeRebind &) override {}
+  void visit (HIR::UseDeclaration &) override {}
+  void visit (HIR::TypeAlias &) override {}
+  void visit (HIR::StructStruct &) override {}
+  void visit (HIR::TupleStruct &) override {}
+  void visit (HIR::EnumItem &) override {}
+  void visit (HIR::EnumItemTuple &) override {}
+  void visit (HIR::EnumItemStruct &) override {}
+  void visit (HIR::EnumItemDiscriminant &) override {}
+  void visit (HIR::Enum &) override {}
+  void visit (HIR::Union &) override {}
+  void visit (HIR::ConstantItem &) override {}
+  void visit (HIR::StaticItem &) override {}
+  void visit (HIR::TraitItemFunc &) override {}
+  void visit (HIR::TraitItemConst &) override {}
+  void visit (HIR::TraitItemType &) override {}
+  void visit (HIR::Trait &) override {}
+  void visit (HIR::ImplBlock &) override {}
+  void visit (HIR::ExternalStaticItem &) override {}
+  void visit (HIR::ExternalFunctionItem &) override {}
+  void visit (HIR::ExternBlock &) override {}
+  void visit (HIR::LiteralPattern &) override {}
+  void visit (HIR::IdentifierPattern &) override {}
+  void visit (HIR::WildcardPattern &) override {}
+  void visit (HIR::RangePatternBoundLiteral &) override {}
+  void visit (HIR::RangePatternBoundPath &) override {}
+  void visit (HIR::RangePatternBoundQualPath &) override {}
+  void visit (HIR::RangePattern &) override {}
+  void visit (HIR::ReferencePattern &) override {}
+  void visit (HIR::StructPatternFieldTuplePat &) override {}
+  void visit (HIR::StructPatternFieldIdentPat &) override {}
+  void visit (HIR::StructPatternFieldIdent &) override {}
+  void visit (HIR::StructPattern &) override {}
+  void visit (HIR::TupleStructItemsNoRange &) override {}
+  void visit (HIR::TupleStructItemsRange &) override {}
+  void visit (HIR::TupleStructPattern &) override {}
+  void visit (HIR::TuplePatternItemsMultiple &) override {}
+  void visit (HIR::TuplePatternItemsRanged &) override {}
+  void visit (HIR::TuplePattern &) override {}
+  void visit (HIR::GroupedPattern &) override {}
+  void visit (HIR::SlicePattern &) override {}
+  void visit (HIR::EmptyStmt &) override {}
+  void visit (HIR::TraitBound &) override {}
+  void visit (HIR::ImplTraitType &) override {}
+  void visit (HIR::TraitObjectType &) override {}
+  void visit (HIR::ParenthesisedType &) override {}
+  void visit (HIR::ImplTraitTypeOneBound &) override {}
+  void visit (HIR::TupleType &) override {}
+  void visit (HIR::NeverType &) override {}
+  void visit (HIR::RawPointerType &) override {}
+  void visit (HIR::ReferenceType &) override {}
+  void visit (HIR::ArrayType &) override {}
+  void visit (HIR::SliceType &) override {}
+  void visit (HIR::InferredType &) override {}
+  void visit (HIR::BareFunctionType &) override {}
+
+  void visit (HIR::AssignmentExpr &expr) override;
+  void visit (HIR::LetStmt &let) override;
+  void visit (HIR::BlockExpr &block) override;
+  void visit (HIR::Function &function) override;
+  void visit (HIR::IdentifierExpr &expr) override;
+  void visit (HIR::ExprStmtWithoutBlock &expr) override;
+  void visit (HIR::ExprStmtWithBlock &expr) override;
 
 private:
   HIR::Polonius polonius;
