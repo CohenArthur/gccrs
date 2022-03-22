7ed2e21a87cd5dcf7751f2e3dcde7677f759cfb7
Arthur Cohen <arthur.cohen@embecosm.com>
2022-03-22 12:38:25 +0100

    tests but THEY ARE BUGGY

--- gcc/testsuite/rust/execute/torture/macros-issue1048-2.rs
+++ gcc/testsuite/rust/execute/torture/macros-issue1048-2.rs
@@ -3,7 +3,8 @@ macro_rules! maybe_return { ($e:expr) => ($e); }
 fn frob(x: i32) -> i32{
     maybe_return! {x}
     // should return 27
-    27
+    27 // { dg-error "expecting .\}. but .integer literal. found" }
+    // { dg-error "error may be from having an expression" "" { target *-*-* } .-1 }
 }
 
 fn main() -> i32 {
--- gcc/testsuite/rust/execute/torture/macros-issue1048.rs
+++ gcc/testsuite/rust/execute/torture/macros-issue1048.rs
@@ -3,7 +3,8 @@ macro_rules! maybe_return { ($e:expr) => ($e); }
 fn frob(x: i32) -> i32{
     maybe_return! {x}
     // should return -1 
-    -1
+    -1 // { dg-error "expecting .\}. but .integer literal. found" }
+    // { dg-error "error may be from having an expression" "" { target *-*-* } .-1 }
 }
 
 fn main() -> i32 {
