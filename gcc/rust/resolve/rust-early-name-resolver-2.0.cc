a4d22684dec2a44f83018d6b6f69c9e1739898eb
Arthur Cohen <arthur.cohen@embecosm.com>
2023-07-25 17:31:35 +0200

    early

--- gcc/rust/resolve/rust-early-name-resolver-2.0.cc
+++ gcc/rust/resolve/rust-early-name-resolver-2.0.cc
@@ -39,17 +39,91 @@ Early::go (AST::Crate &crate)
   rust_debug ("[ARTHUR] toplevel macros:\n%s",
 	      resolver.macros.as_debug_string ().c_str ());
 
+  // FIXME: Do we add a `scoped` method?
+  textual_scope.push ();
+
   // Then we proceed to the proper "early" name resolution: Import and macro
   // name resolution
   for (auto &item : crate.items)
     item->accept_vis (*this);
+
+  textual_scope.pop ();
+}
+
+void
+Early::TextualScope::push ()
+{
+  // push a new empty scope
+  scopes.emplace_back ();
+}
+
+void
+Early::TextualScope::pop ()
+{
+  rust_assert (!scopes.empty ());
+
+  scopes.pop_back ();
+}
+
+void
+Early::TextualScope::insert (std::string name, NodeId id)
+{
+  rust_assert (!scopes.empty ());
+
+  // FIXME: Do not ignore return value
+  scopes.back ().insert ({name, id});
+
+  rust_unreachable ();
+}
+
+tl::optional<NodeId>
+Early::TextualScope::get (const std::string &name)
+{
+  for (auto iterator = scopes.rbegin (); iterator != scopes.rend (); iterator++)
+    {
+      auto scope = *iterator;
+      auto found = scope.find (name);
+      if (found != scope.end ())
+	return found->second;
+    }
+
+  return tl::nullopt;
+}
+
+void
+Early::visit (AST::MacroRulesDefinition &def)
+{
+  DefaultResolver::visit (def);
+
+  textual_scope.insert (def.get_rule_name ().as_string (), def.get_node_id ());
 }
 
 void
 Early::visit (AST::MacroInvocation &invoc)
 {
-  auto definition
-    = resolver.macros.resolve_path (invoc.get_invoc_data ().get_path ());
+  auto path = invoc.get_invoc_data ().get_path ();
+
+  // When a macro is invoked by an unqualified identifier (not part of a
+  // multi-part path), it is first looked up in textual scoping. If this does
+  // not yield any results, then it is looked up in path-based scoping. If the
+  // macro's name is qualified with a path, then it is only looked up in
+  // path-based scoping.
+
+  // https://doc.rust-lang.org/reference/macros-by-example.html#path-based-scope
+
+  tl::optional<NodeId> definition = tl::nullopt;
+  if (path.get_segments ().size () == 1)
+    {
+      auto text_def
+	= textual_scope.get (path.get_final_segment ().as_string ());
+
+      if (!text_def)
+	definition = resolver.macros.resolve_path (path);
+    }
+  else
+    {
+      definition = resolver.macros.resolve_path (path);
+    }
 
   if (!definition.has_value ())
     {
--- gcc/rust/resolve/rust-early-name-resolver-2.0.h
+++ gcc/rust/resolve/rust-early-name-resolver-2.0.h
@@ -19,6 +19,7 @@
 #ifndef RUST_EARLY_NAME_RESOLVER_2_0_H
 #define RUST_EARLY_NAME_RESOLVER_2_0_H
 
+#include "optional.h"
 #include "rust-ast.h"
 #include "rust-ast-visitor.h"
 #include "rust-name-resolver-2.0.h"
@@ -36,11 +37,29 @@ public:
 
   void go (AST::Crate &crate);
 
+  void visit (AST::MacroRulesDefinition &) override;
   void visit (AST::MacroInvocation &) override;
   void visit (AST::UseDeclaration &) override;
   void visit (AST::UseTreeRebind &) override;
   void visit (AST::UseTreeList &) override;
   void visit (AST::UseTreeGlob &) override;
+
+private:
+  // TODO: Add documentation that this is needed...
+  class TextualScope
+  {
+  public:
+    void push ();
+    void pop ();
+
+    void insert (std::string name, NodeId id);
+    tl::optional<NodeId> get (const std::string &name);
+
+  private:
+    std::vector<std::unordered_map<std::string, NodeId>> scopes;
+  };
+
+  TextualScope textual_scope;
 };
 
 } // namespace Resolver2_0
