0a0e057e87605b5dc17f1d94f037b839df6d10b9
Arthur Cohen <arthur.cohen@embecosm.com>
2026-05-28 17:33:50 +0200

    Revert "gccrs: Disambiguate identifier and path patterns"
    This reverts commit 9d80cc3487731ccbee7e76141aee25bc90f1cec4.

--- gcc/rust/util/rust-hir-map.h
+++ gcc/rust/util/rust-hir-map.h
@@ -357,9 +357,6 @@ public:
   void add_derived_node (NodeId node_id);
   bool is_derived_node (NodeId node_id);
 
-  void add_function_node (NodeId node_id);
-  bool is_function_node (NodeId node_id);
-
 private:
   Mappings ();
 
@@ -460,8 +457,6 @@ private:
   std::unordered_map<NodeId, std::vector<NodeId>> captures;
 
   std::set<NodeId> derived_nodes;
-
-  std::set<NodeId> function_nodes;
 };
 
 } // namespace Analysis
